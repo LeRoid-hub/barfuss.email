964ba57f6e9c1818d34962c9b49550381f4a7eb9
Jan Barfuß <janabr2001@gmail.com>
2023-11-05 02:18:30 +0100

    first templates

---
+++ barfuss-website/src/components/Index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TextItem from './TextItem.vue'
+</script>
+
+<template>
+    <TextItem>
+        <template #heading> TEST </template>
+        Dies ist ein Test!
+    </TextItem>
+
+    <TextItem>
+        <template #heading> TEST </template>
+        Dies ist ein Test!
+    </TextItem>
+    <TextItem>
+        <template #heading> TEST </template>
+        Dies ist ein Test!
+    </TextItem>
+
+
+</template>
--- barfuss-website/src/views/HomeView.vue
+++ barfuss-website/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/Index.vue'
 </script>
 
 <template>
